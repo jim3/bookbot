8f262772b80db1d103321fc621e9b0047830c37f
jim3 <jimmy3.xyz@gmail.com>
2025-04-01 10:04:42 -0400

    Update files

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import sys
 from stats import get_num_characters, get_num_words, print_report
 
+NUM_ARGS = 2
+
 def get_book_text(filepath: str) -> str:
     with open(filepath, "r") as f:
         text = f.read()
     return text
 
 def main() -> None:
-    if len(sys.argv) != 2:
+    if len(sys.argv) != NUM_ARGS:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
 
     book_path = sys.argv[1]
     text = get_book_text(book_path)
+    numwords = get_num_words(text)
     char_counts = get_num_characters(text)
     report = print_report(char_counts)
     
