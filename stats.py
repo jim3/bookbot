553f042318407b6cf5c78902d94c61015ea61cdc
jim3 <jimmy3.xyz@gmail.com>
2025-04-01 10:08:23 -0400

    Update files

--- stats.py
+++ stats.py
@@ -1,9 +1,11 @@
 import re
 
+
 # Get number of words in a text
 def get_num_words(booktxt: str) -> int:
     num_words = len(booktxt.split())
     return num_words
+
 
 def get_num_characters(text: str) -> dict[str, int]:
     lowered_text = text.lower()
@@ -15,14 +17,16 @@ def get_num_characters(text: str) -> dict[str, int]:
             char_count[c] = 1
     return char_count
 
+
 def sort_on(dict):
     return dict["num"]
+
 
 def print_report(d: dict[str, int]):
     report = []
     char = d.copy()
     for k, v in list(char.items()):
-        char_count = {"name": k, "num": v}        
+        char_count = {"name": k, "num": v}
         report.append(char_count)
     report.sort(reverse=True, key=sort_on)
     return report
