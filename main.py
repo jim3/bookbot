75aa1deda812bc1752f92e4dcec092039fb27cc2
jim3 <jimmy3.xyz@gmail.com>
2025-04-01 10:08:04 -0400

    Update files

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from stats import get_num_characters, get_num_words, print_report
 
 NUM_ARGS = 2
 
+
 def get_book_text(filepath: str) -> str:
     with open(filepath, "r") as f:
         text = f.read()
     return text
+
 
 def main() -> None:
     if len(sys.argv) != NUM_ARGS:
@@ -18,10 +20,11 @@ def main() -> None:
     numwords = get_num_words(text)
     char_counts = get_num_characters(text)
     report = print_report(char_counts)
-    
+
     for r in report:
-        if r['name'].isalpha():
+        if r["name"].isalpha():
             print(f"{r['name']}: {r['num']}")
+
 
 if __name__ == "__main__":
     main()
